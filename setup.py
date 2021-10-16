74bb669afb157e310b075f01aa75346ba9b3048c
Tejas Balshetwar <66072201+TejasBalshetwar@users.noreply.github.com>
2021-10-16 16:49:18 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PONG',
+    version='0.1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='tejas',
+    author_email='',
+    description=''
+)
